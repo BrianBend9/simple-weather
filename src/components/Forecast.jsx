ecf5d30fee3bd4aaabf70545b4d8cf3e1d6f337b
BrianBend9 <BrianBend9@users.noreply.github.com>
2016-08-22 22:20:06 -0400

    Added function to data helpers to provide forecast component with a formatted object array.

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-/* eslint-disable react/prop-types */
 /* eslint-disable react/no-set-state */
+/* eslint-disable react/prop-types*/
 import React from 'react';
 import {getWeatherData} from '../utils/DataHelpers';
 
@@ -8,8 +8,14 @@ export default class ForecastComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentForecat: '',
-      fiveDayForecast: ''
+      city: '',
+      country: '',
+      currentForecast: '',
+      0: '',
+      1: '',
+      2: '',
+      3: '',
+      4: ''
     };
   }
 
@@ -17,25 +23,21 @@ export default class ForecastComponent extends React.Component {
     var currentPath = this.props.location.pathname;
     var city = currentPath.slice(currentPath.match(/\/\w*\//)[0].length);
 
-    getWeatherData(city).then(dataObject => {
-      console.log(dataObject);
-      this.setState({
-        currentForecat: (dataObject[1].list) ? dataObject[0] : dataObject[1].list,
-        fiveDayForcast: (dataObject[1].list) ? dataObject[1].list : dataObject[0]
-      });
+    getWeatherData(city).then(dataObjectsArray => {
+      dataObjectsArray.forEach(dataObject => this.setState(dataObject));
     });
-
   }
 
   render() {
     return (
       <div className='dataContainer'>
         <div className='dataContainer--current'>
-          <h1 className='cityHeader'>{'ForecastComponent Container'}</h1>
-          <div className="dataDisplay--large">{'div1'}</div>
+          <h1 className='cityHeader'>{this.state.city}</h1>
+          <h2>{'Select a day'}</h2>
+          <div className="dataDisplay--large">{'div1'} </div>
         </div>
         <div className='dataContainer--5day'>
-          <div className="dataDisplay">{'div2'}</div>
+          <div className="dataDisplay">{this.state[0].description}</div>
           <div className="dataDisplay">{'div3'}</div>
           <div className="dataDisplay">{'div4'}</div>
           <div className="dataDisplay">{'div5'}</div>
--- src/utils/DataHelpers.jsx
+++ src/utils/DataHelpers.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable handle-callback-err */
+/* eslint-disable consistent-return */
 require('es6-promise').polyfill();
 
 function getData(url, apikey) {
@@ -27,6 +28,61 @@ function getData(url, apikey) {
   });
 }
 
+function dayOfWeek(unixTimestamp) {
+  const date = new Date(unixTimestamp * 1000);
+  const day = date.getUTCDay();
+
+  switch (day) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+  }
+}
+
+function createDataObjectsArray(dataObject) {
+  const currentForecast = dataObject[0];
+  const fiveDayForecast = dataObject[1].list;
+  var dataObjectsArray = [];
+  var day, obj;
+
+  dataObjectsArray.push({'city': currentForecast.name});
+  dataObjectsArray.push({'country': currentForecast.sys.country});
+  dataObjectsArray.push({currentForecast: {
+    description: currentForecast.weather[0].description,
+    icon: currentForecast.weather[0].icon,
+    humidity: currentForecast.main.humidity,
+    wind: currentForecast.wind.speed,
+    temp: currentForecast.main.temp,
+    lowTemp: currentForecast.main.temp_min,
+    highTemp: currentForecast.main.temp_max
+  }});
+
+  for (day in fiveDayForecast) {
+    obj = {};
+    obj[day] = {};
+    obj[day]['fullDate'] = (new Date(fiveDayForecast[day].dt * 1000)).toDateString();
+    obj[day]['dayOfWeek'] = dayOfWeek(fiveDayForecast[day].dt);
+    obj[day]['description'] = fiveDayForecast[day].weather[0].description;
+    obj[day]['icon'] = fiveDayForecast[day].weather[0].icon;
+    obj[day]['humidity'] = fiveDayForecast[day].humidity;
+    obj[day]['wind'] = fiveDayForecast[day].speed;
+    obj[day]['temp'] = fiveDayForecast[day].temp.day;
+    obj[day]['lowTemp'] = fiveDayForecast[day].temp.min;
+    obj[day]['highTemp'] = fiveDayForecast[day].temp.max;
+    dataObjectsArray.push(obj);
+  }
+  return dataObjectsArray;
+}
+
 function getWeatherData(city) {
   const apikey = '06e4d406550bf413c913e25583660216';
   const fiveDayForcastUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' +
@@ -41,6 +97,8 @@ function getWeatherData(city) {
     return dataObjectArray.map(function(dataObject) {
       return JSON.parse(dataObject);
     });
+  }).then(function(dataObject) {
+    return createDataObjectsArray(dataObject);
   });
 }
 
